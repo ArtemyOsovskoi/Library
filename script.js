9de27b99803194b953fd393360eaa4d0ed8b29ee
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2023-10-24 11:12:40 +0300

    made constructor and now it can work with an array, pushing new object and storing it there

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
-//store book object inside an array
+//book objects array
 const myLibrary = [];
 
 //the constructor function
-function Book(Title, Author, Pages, Status) {
-    this.Title = Title
-    this.Author = Author
-    this.Pages = Pages
-    this.Status = Status
+function Book(title, author, pages, status) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
 }
 
+let newBook = new Book('1', '2', '3', '4');
 
-//store the new book object into an array
+//take input and store new book object into an array
 function addBookToLibrary() {
-    const newBook = new Book(prompt('Title'), prompt('Author'), prompt('Pages'), prompt('Status'));
     myLibrary.push(newBook);
 }
+addBookToLibrary();
 
 
-/* const LOTRpOne = new Book('the lord of 
-the rings p.1', 'J.R.R. Tolkien','423 pages',
-'currently reading');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 23.10.23 после пары часов работы, понял что надо снова почитать теорию и вернутся
+    1) Перечитать урок про объекты и конструкторы, конспект см. и вспомнить как хранить и создавать объекты.
+    2) Перечитать урок про формы и вспомнить как использовать их чтобы взять данные пользователя.
  */
+
+//comment from Discord: 
+/* I suggest couple of things, read the lessons for objects and constructors, 
+this is not good way of storing an object/creating an object, using prompt.
+
+Also an advice, don't start with DOM immediately, think how you can make this work in the console 
+and after it's working start using DOM.
+
+Secondly re-read the lesson with forms, this way you will know how to use forms to take user input.
+
+After you created the objects, think of way to show the books, 
+i think in the assignment there as an hint to show each book in a card or table, 
+if you know DOM you can easily do this, and it might seem repetitive, 
+so maybe it could be good way to use something, that could do that for each book.
+
+And if have any questions, ask regarding the project, but re-reading these lessons, 
+could help you from being stuck. */
 
 /* Алгоритм 
 
