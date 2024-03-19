f4033005edc19df540236aac74b3f11ce8344e75
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2024-03-19 15:36:23 +0300

    refactor with Classes

--- script.js
+++ script.js
@@ -1,12 +1,106 @@
-//book objects array
 let myLibrary = [];
 
-//the constructor function
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages + "p.";
-    this.status = status;
+class Book {
+    constructor (title, author, pages, status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+    //create DOM book card elements
+    createBook(object) {
+        let removeButton = document.createElement("button");
+        removeButton.addEventListener("click", (book) => {
+            //clear DOM
+            book.currentTarget.closest("div").remove();
+            //remove book from array
+            myLibrary.splice(myLibrary.indexOf(object), 1);
+        });
+        removeButton.textContent = "Remove";
+        removeButton.classList.add("removeButton");
+
+        let changeStatusButton = document.createElement("button");
+        changeStatusButton.addEventListener("click", () => {
+                //change book status       
+            let cardBookStatus = document.querySelector(".cardBookStatus");
+            if (object.status === "Read") {
+                cardBookStatus.textContent = "Not read";
+                object.status = "Not read";           
+            } else { 
+                cardBookStatus.textContent = "Read";
+                object.status = "Read";
+            } 
+            //run display function to refresh
+            this.displayBook();
+
+        });
+        changeStatusButton.textContent = "Change status";
+        changeStatusButton.classList.add("changeStatusButton");
+
+        //create card html elements
+        let container = document.createElement("div");
+        container.classList.add("bookCard");
+        //set data attribute corresponding to the library array index
+        //container.setAttribute("data-bookIndex", [i]); 
+        container.style.textAlign = "center";
+
+        let cardTitle = document.createElement("h3");
+        let cardAuthor = document.createElement("p");
+        let cardPages = document.createElement("p");
+        let cardStatus = document.createElement("p");
+        cardStatus.classList.add("cardBookStatus");
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("checkbox");
+        checkbox.style.visibility = "hidden";
+        let checkboxLabel = document.createElement("label");
+        checkboxLabel.textContent = "Reading now";
+        checkboxLabel.style.visibility = "hidden";
+
+        //card content
+        let titleContent = document.createTextNode(object.title);
+        let authorContent = document.createTextNode(object.author);
+        let pagesContent = document.createTextNode(object.pages);
+        let statusContent = document.createTextNode(object.status);
+        
+        cardTitle.appendChild(titleContent);
+        cardAuthor.appendChild(authorContent);
+        cardPages.appendChild(pagesContent);
+        cardStatus.appendChild(statusContent);
+        
+        container.appendChild(cardTitle);
+        container.appendChild(cardAuthor);
+        container.appendChild(cardPages);
+        container.appendChild(checkboxLabel);
+        container.appendChild(checkbox);
+        container.appendChild(cardStatus);
+        container.appendChild(removeButton);
+        container.appendChild(changeStatusButton);
+
+        allBooks.appendChild(container);
+
+        if (object.status === "Not read") {
+            checkbox.style.visibility = "visible";
+            checkboxLabel.style.visibility = "visible";
+        } else {
+            checkbox.style.visibility = "hidden";
+            checkboxLabel.style.visibility = "hidden";
+        }
+    }
+    //loop through the array and display each book as a card on the page
+    displayBook() {
+        //remove repeating book elements
+        const allBooks = document.getElementById("allBooks");
+        const bookCards = document.querySelectorAll('.bookCard'); 
+        bookCards.forEach(book => {
+            allBooks.removeChild(book);
+        });
+        //execute createBook() until less than array length
+        for(let i = 0; i < myLibrary.length; i++ ) {
+            //pass array element (book object) as an argument to createBook function
+            this.createBook(myLibrary[i]);
+        }
+    }
 }
 
 //take input and store new book object into an array
@@ -22,105 +116,9 @@ addBookButton.addEventListener("click", () => {
     //store new book object to library array
     myLibrary.push(newBook);
     //display new book object on the page
-    displayBook(title, author, pages, status);
+    newBook.displayBook(title, author, pages, status);
     
 }); 
-
-//loop through the array and display each book as a card on the page
-function displayBook() {
-    //remove repeating book elements
-    const allBooks = document.getElementById("allBooks");
-    const bookCards = document.querySelectorAll('.bookCard'); 
-    bookCards.forEach(book => {
-        allBooks.removeChild(book);
-    });
-    //execute createBook() until less than array length
-    for(i = 0; i < myLibrary.length; i++ ) {
-        //pass array element (book object) as an argument to createBook function
-        createBook(myLibrary[i]);
-    }
-}
-
-//create DOM book card elements
-function createBook(object) {
-    let removeButton = document.createElement("button");
-    removeButton.addEventListener("click", (book) => {
-        //clear DOM
-        book.currentTarget.closest("div").remove();
-        //remove book from array
-        myLibrary.splice(myLibrary.indexOf(object), 1);
-    });
-    removeButton.textContent = "Remove";
-    removeButton.classList.add("removeButton");
-
-    let changeStatusButton = document.createElement("button");
-    changeStatusButton.addEventListener("click", () => {
-            //change book status       
-        let cardBookStatus = document.querySelector(".cardBookStatus");
-        if (object.status === "Read") {
-            cardBookStatus.textContent = "Not read";
-            object.status = "Not read";           
-        } else { 
-            cardBookStatus.textContent = "Read";
-            object.status = "Read";
-        } 
-        //run display function to refresh
-        displayBook();
-
-    });
-    changeStatusButton.textContent = "Change status";
-    changeStatusButton.classList.add("changeStatusButton");
-
-    //create card html elements
-    let container = document.createElement("div");
-    container.classList.add("bookCard");
-        //set data attribute corresponding to the library array index
-    //container.setAttribute("data-bookIndex", [i]); 
-    container.style.textAlign = "center";
-
-    let cardTitle = document.createElement("h3");
-    let cardAuthor = document.createElement("p");
-    let cardPages = document.createElement("p");
-    let cardStatus = document.createElement("p");
-    cardStatus.classList.add("cardBookStatus");
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList.add("checkbox");
-    checkbox.style.visibility = "hidden";
-    let checkboxLabel = document.createElement("label");
-    checkboxLabel.textContent = "Reading now";
-    checkboxLabel.style.visibility = "hidden";
-
-    //card content
-    let titleContent = document.createTextNode(object.title);
-    let authorContent = document.createTextNode(object.author);
-    let pagesContent = document.createTextNode(object.pages);
-    let statusContent = document.createTextNode(object.status);
-    
-    cardTitle.appendChild(titleContent);
-    cardAuthor.appendChild(authorContent);
-    cardPages.appendChild(pagesContent);
-    cardStatus.appendChild(statusContent);
-    
-    container.appendChild(cardTitle);
-    container.appendChild(cardAuthor);
-    container.appendChild(cardPages);
-    container.appendChild(checkboxLabel);
-    container.appendChild(checkbox);
-    container.appendChild(cardStatus);
-    container.appendChild(removeButton);
-    container.appendChild(changeStatusButton);
-
-    allBooks.appendChild(container);
-
-    if (object.status === "Not read") {
-        checkbox.style.visibility = "visible";
-        checkboxLabel.style.visibility = "visible";
-    } else {
-        checkbox.style.visibility = "hidden";
-        checkboxLabel.style.visibility = "hidden";
-    }
-}
 
 //show form window - input the details for the new book
 const showFormWindow = document.getElementById("formPopup");
